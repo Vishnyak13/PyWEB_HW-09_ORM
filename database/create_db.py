bb56c98ac0389315025caf5361be64b61791fcd9
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-11-26 17:13:40 +0200

    fix echo in engine

--- database/create_db.py
+++ database/create_db.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 URL_DB = 'sqlite:///addressbook.db'
 
-engine = create_engine(URL_DB, echo=True)
+engine = create_engine(URL_DB, echo=False)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
